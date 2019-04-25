8fb28665052c41d5bca4467e6464b60dd31d1e04
Admin9705 <24727006+Admin9705@users.noreply.github.com>
2019-04-24 20:29:38 -0400

    update

---
+++ menu/functions/install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#
+# Title:      PGBlitz (Reference Title File)
+# Author(s):  Admin9705
+# URL:        https://pgblitz.com - http://github.pgblitz.com
+# GNU:        General Public License v3.0
+################################################################################
+
+# This executes pgblitz for users upgrading from 8.6 and below
+bash /opt/plexguide/functions/install.sh
