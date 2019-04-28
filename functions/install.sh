46e54078eb16f990bf0d7c04ca2858df28cbe049
Admin9705 <24727006+Admin9705@users.noreply.github.com>
2019-04-28 09:55:29 -0400

    update

--- functions/install.sh
+++ functions/install.sh
@@ -129,3 +129,64 @@ prune () { ansible-playbook /opt/plexguide/menu/prune/main.yml }
 # Roles Ensure that PG Replicates and has once if missing; important for startup, cron and etc
 pgcore() { if [ ! -e "/opt/coreapps/place.holder" ]; then ansible-playbook /opt/plexguide/menu/pgbox/pgboxcore.yml; fi }
 pgcommunity() { if [ ! -e "/opt/communityapps/place.holder" ]; then ansible-playbook /opt/plexguide/menu/pgbox/pgboxcommunity.yml; fi }
+
+sudocheck() {
+if [[ $EUID -ne 0 ]]; then
+tee <<-EOF
+
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+⛔️  You Must Execute as a SUDO USER (with sudo) or as ROOT!
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+
+EOF
+exit 1
+fi
+}
+
+downloadpg() {
+  rm -rf /opt/plexguide
+  git clone https://github.com/PGBlitz/PGBlitz.com.git /opt/plexguide && cp /opt/plexguide/menu/interface/alias/templates/plexguide /bin/
+  cp /opt/plexguide/menu/interface/alias/templates/plexguide /bin/plexguide
+}
+
+missingpull() {
+file="/opt/plexguide/menu/functions/install.sh"
+if [ ! -e "$file" ]; then
+tee <<-EOF
+
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+⛔️  /opt/plexguide went missing!
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+
+EOF
+  sleep 2
+tee <<-EOF
+
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+ 🍖  NOM NOM - Re-Downloading PGBlitz for BoneHead User!
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+EOF
+sleep 2
+  downloadpg
+tee <<-EOF
+
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+✅️  Repair Complete! Standby!
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+
+EOF
+  sleep 2
+  fi
+}
+
+exitcheck() {
+bash /opt/plexguide/menu/version/file.sh
+file="/pg/var/exited.upgrade"
+if [ ! -e "$file" ]; then
+	bash /opt/plexguide/menu/interface/ending.sh
+else
+	rm -rf /pg/var/exited.upgrade 1>/dev/null 2>&1
+	echo ""
+	bash /opt/plexguide/menu/interface/ending.sh
+fi
+}
